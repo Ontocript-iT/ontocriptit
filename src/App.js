d0a1bd9fcd1b60d2946e93eea8dcf5497dd9c519
thilina22 <test@123gmail.com>
2024-02-09 21:00:05 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
+import Home from './components/pages/home/Home';
+import Header from './components/shared/header/Header';
+import Footer from "./components/shared/footer/Footer";
 
+import { createBrowserRouter, Route,createRoutesFromElements } from 'react-router-dom';
 
-import Header from "./components/shared/header/Header";
 import Process from "./components/pages/process/Process";
 
 const router = createBrowserRouter(
