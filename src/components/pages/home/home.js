bda87416ce2729083e19313845cae4b6e7e49dc2
Vishwa Waweliyadda <waweliyadda.1998@gmail.com>
2024-02-11 10:32:15 +0530

    resolved styles conflicts

--- src/components/pages/home/home.js
+++ src/components/pages/home/home.js
@@ -14,11 +14,11 @@ function Home() {
     <div>
       <Hero />
       <Service />
-      <WhyUs />
+      {/* <WhyUs />
       <TechStacks />
       <Contact />
       <OurWorks />
-      <OurTeam />
+      <OurTeam /> */}
     </div>
 
   );
