77edc065d88c9f0e2cb435a3edcd3e044f7a9b3b
thilina22 <test@123gmail.com>
2024-02-20 21:57:07 +0530

    updated about us

---
+++ src/components/pages/aboutUs/leardershipTeam/LeadershipTeam.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import "./LeaderShipTeam.css";
+
+export default function LeadershipTeam() {
+  return (
+    <div className='team-main-container'>
+
+        <div className='text-container'>
+
+            <h1 className='topic'>Our Leadership</h1>
+            <p className='sub-text'>Under our leadership, we make new things, do them really well, and help businesses move forward confidently and successfully</p>
+
+        </div>
+        
+      
+    </div>
+  )
+}
