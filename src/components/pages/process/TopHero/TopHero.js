6d82d951013d486818784982b3afdcab381843f8
thilina22 <test@123gmail.com>
2024-02-23 21:08:57 +0530

    updated process

---
+++ src/components/pages/process/TopHero/TopHero.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import "./TopHero.css";
+
+export default function TopHero() {
+  return (
+    <div className='container'>
+    <div className='textContainer'>
+        
+    <h3>See</h3>
+    <h1 className='mainText'>ONTOCRIPT iT PROCESS</h1>
+
+
+    </div>
+   
+
+   
+
+  
+</div>
+  )
+}
