657ddd00c72d757a8a32ed24f0581a01259b866e
thilina22 <test@123gmail.com>
2024-02-23 21:59:40 +0530

    created privacy page

---
+++ src/components/pages/Privacy-Policy/PrivacyPolicy.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import TopHero from './TopHero/TopHero'
+import Footer from '../../shared/footer/Footer'
+import PrivacyAndPolicies from './PrivacyAndPolicy/PrivacyAndPolicies'
+
+export default function PrivacyPolicy() {
+  return (
+    <div>
+        <TopHero/>
+        <PrivacyAndPolicies/>
+        
+        <Footer/>
+      
+    </div>
+  )
+}
---
+++ src/components/pages/Technology/Technology.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import TopHero from './TopHero/TopHero'
+import Footer from '../../shared/footer/Footer'
+
+export default function Technology() {
+  return (
+    <div>
+        <TopHero/>
+        <Footer/>
+      
+    </div>
+  )
+}
