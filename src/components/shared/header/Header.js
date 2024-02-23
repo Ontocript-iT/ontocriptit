510ed08eecda52bb07ee172c4d84d4535a4d0adf
thilina22 <test@123gmail.com>
2024-02-23 21:13:56 +0530

    Update Header.js

--- src/components/shared/header/Header.js
+++ src/components/shared/header/Header.js
@@ -3,6 +3,7 @@ import { AppBar, Toolbar, Button } from "@mui/material";
 import "./Header.css";
 import logo from "./ontocript.png"; // Assuming NavigationBar.js is in the components directory
 import { Outlet } from "react-router-dom";
+import { Link } from 'react-router-dom'; 
 
 const styles = {
   appBar: {
@@ -15,7 +16,9 @@ function Header() {
     <AppBar position="stastic" style={styles.appBar}>
       <nav>
         <div className="logo">
+        <Link to="/">
           <img src={logo} alt="Logo" />
+          </Link>
         </div>
         <div className="headerLinks">
         <ul>
