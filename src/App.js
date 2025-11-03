1253d9e33d16224945694a2519f8b7e5b9bbd04c
thilina22 <test@123gmail.com>
2025-11-03 11:07:19 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,11 +1,47 @@
+// import Home from "./components/pages/home/Home";
+// import Header from "./components/shared/header/Header";
+// import {
+//   RouterProvider,
+//   Route,
+//   createBrowserRouter,
+//   createRoutesFromElements,
+// } from "react-router-dom";
+// import ContactUs from "./components/pages/contactus/ContactUs";
+// import Service from "./components/pages/services/Service";
+// import Blog from "./components/pages/Blog/Blog";
+// import AboutUs from "./components/pages/aboutUs/AboutUs";
+// import Technology from "./components/pages/Technology/Technology";
+// import PrivacyPolicy from "./components/pages/Privacy-Policy/PrivacyPolicy";
+// import BlogData from "./components/pages/Blog/BlogData";
+// import Portfolio from "./components/pages/portfolio/Portfolio";
+
+// const router = createBrowserRouter(
+//   createRoutesFromElements(
+//     <Route path="/" element={<Header />}>
+//       <Route index element={<Home />} />
+//       {/* <Route path="/process" element={<Process />} /> */}
+//       <Route path="/contact" element={<ContactUs />} />
+//       <Route path="/services" element={<Service />} />
+//       <Route path="/blog" element={<Blog />} />
+//       <Route path="/blogData/:id" element={<BlogData />} />
+//       <Route path="/aboutus" element={<AboutUs />} />
+//       <Route path="/technology" element={<Technology />} />
+//       <Route path="/privacy-policy" element={<PrivacyPolicy />} />
+//       <Route path="/portfolio" element={<Portfolio />} />
+//     </Route>
+//   ),
+//   // { basename: "/ontocriptit" }
+// );
+// function App() {
+//   return <RouterProvider router={router} />;
+// }
+
+// export default App;
+
+
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Home from "./components/pages/home/Home";
 import Header from "./components/shared/header/Header";
-import {
-  RouterProvider,
-  Route,
-  createBrowserRouter,
-  createRoutesFromElements,
-} from "react-router-dom";
 import ContactUs from "./components/pages/contactus/ContactUs";
 import Service from "./components/pages/services/Service";
 import Blog from "./components/pages/Blog/Blog";
@@ -15,26 +51,24 @@ import PrivacyPolicy from "./components/pages/Privacy-Policy/PrivacyPolicy";
 import BlogData from "./components/pages/Blog/BlogData";
 import Portfolio from "./components/pages/portfolio/Portfolio";
 
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path="/" element={<Header />}>
-      <Route index element={<Home />} />
-      {/* <Route path="/process" element={<Process />} /> */}
-      <Route path="/contact" element={<ContactUs />} />
-      <Route path="/services" element={<Service />} />
-      <Route path="/blog" element={<Blog />} />
-      <Route path="/blogData/:id" element={<BlogData />} />
-      <Route path="/aboutus" element={<AboutUs />} />
-      <Route path="/technology" element={<Technology />} />
-      <Route path="/privacy-policy" element={<PrivacyPolicy />} />
-      <Route path="/portfolio" element={<Portfolio />} />
-    </Route>
-  ),
-  // { basename: "/ontocriptit" }
-);
 function App() {
-  return <RouterProvider router={router} />;
+  return (
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<Header />}>
+          <Route index element={<Home />} />
+          <Route path="/contact" element={<ContactUs />} />
+          <Route path="/services" element={<Service />} />
+          <Route path="/blog" element={<Blog />} />
+          <Route path="/blogData/:id" element={<BlogData />} />
+          <Route path="/aboutus" element={<AboutUs />} />
+          <Route path="/technology" element={<Technology />} />
+          <Route path="/privacy-policy" element={<PrivacyPolicy />} />
+          <Route path="/portfolio" element={<Portfolio />} />
+        </Route>
+      </Routes>
+    </HashRouter>
+  );
 }
 
 export default App;
-
